8d102a076075fb63aa94f27e97b920f1f1e61931
Misbah Ansori <misbah.ansori24@gmail.com>
2022-06-07 07:03:04 +0700

    Fix

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export default function obfuscateTailwind(config: {}) {
   const rawClassesMap = new Map();
   return {
     name: "obfuscate-tailwind",
+    apply: "build",
     transform: (code: string, id: string) => {
       if (id.endsWith(".vue") && !id.startsWith("vendor")) {
         const rawClasses = code
@@ -70,15 +71,17 @@ export default function obfuscateTailwind(config: {}) {
           .match(cssClassNameRegex)
           .map((className) => className.slice(1, -1));
 
-        cssClassNames.forEach((className) => {
-          const escapedClassName = escapeClassName(className);
-          if (classMap.has(className.replace(/\\/gi, ""))) {
-            code = code.replace(
-              new RegExp(`\.${escapedClassName}{`, "g"),
-              "." + classMap.get(className.replace(/\\/gi, "")) + "{"
-            );
-          }
-        });
+        cssClassNames
+          .sort((a, b) => b.length - a.length)
+          .forEach((className) => {
+            const escapedClassName = escapeClassName(className);
+            if (classMap.has(className.replace(/\\/gi, ""))) {
+              code = code.replace(
+                new RegExp(`\.${escapedClassName}{`, "g"),
+                "." + classMap.get(className.replace(/\\/gi, "")) + "{"
+              );
+            }
+          });
 
         return {
           code,
