afec8fad1325902467213de02288e06bda6e40e5
Misbah Ansori <misbah.ansori24@gmail.com>
2022-06-08 20:38:06 +0700

    Feat : Add a new class regex for tenary syntac

--- src/modules/vue.ts
+++ src/modules/vue.ts
@@ -5,17 +5,25 @@ export default function vue(
   classMapping: Map<string, string>,
   config: GeneratorConfig
 ) {
-  const classRegexs = [/class="([a-z-0-9\\\[\]\/\(\)\.': ]*)"/g];
-  const rawClassesMap = new Map();
-  const rawClasses: string[] = [];
+  const classRegexs = [
+    // Catch regular class names
+    /class="([a-z-0-9\\\[\]\/\(\)\.': ]*)"/g,
+
+    // Catch class names using tenary operator
+    /:class="[\s\S]*\?(?: *)?'([a-z-0-9\\\[\]\/\(\)\.: ]*)'(?: *)?[\s\S]*:(?: *)?'([a-z-0-9\\\[\]\/\(\)\.: ]*)'/g,
+  ];
+
+  let rawClasses: string[] = [];
 
   classRegexs.forEach((regex) => {
     let match: RegExpExecArray;
     while ((match = regex.exec(code)) !== null) {
       rawClasses.push(match[1]);
+      rawClasses.push(match[2]);
     }
   });
 
+  rawClasses = rawClasses.filter(Boolean);
   const classes = rawClasses.map((c) => c.split(" "));
 
   const unqiueClasses = new Set(classes.flat());
@@ -27,6 +35,8 @@ export default function vue(
     }
     classMapping.set(className, random);
   });
+
+  const rawClassesMap = new Map();
 
   rawClasses
     .sort((a, b) => b.length - a.length)
